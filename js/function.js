b84b094b9a010a3e6cd1132b8db84ef3ea13b59e
simon <semyon.dudin@inbox.ru>
2024-10-31 21:06:46 +0300

    Функции возвращаются

--- js/function.js
+++ js/function.js
@@ -49,3 +49,27 @@ parseNumber('2023 год'); // 2023
 // parseNumber(2023); // 2023
 // parseNumber(-1);   // 1
 // parseNumber(1.5);  // 15
+
+// Task 4
+
+const timeInMinuters = (time) => {
+  const arrayTime = time.split(':');
+  const minuters = arrayTime[0] * 60 + Number(arrayTime[1]);
+  return minuters;
+};
+
+const checkMeet = (startDay, endDay, startMeet, timeMeet) => timeInMinuters(startMeet) + timeMeet >= timeInMinuters(startDay) && timeInMinuters(startMeet) + timeMeet <= timeInMinuters(endDay);
+
+checkMeet('08:00', '17:30', '14:00', 90);
+
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+// имяФункции('08:00', '17:30', '14:00', 90); // true
+// имяФункции('8:0', '10:0', '8:0', 120);     // true
+// имяФункции('08:00', '14:30', '14:00', 90); // false
+// имяФункции('14:00', '17:30', '08:0', 90);  // false
+// имяФункции('8:00', '17:30', '08:00', 900); // false
