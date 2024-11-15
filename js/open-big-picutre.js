3df2f1e2c395637bb3e8edc9d933b3688c5c3739
simon <semyon.dudin@inbox.ru>
2024-11-15 21:55:41 +0300

    мелкие правки

--- js/open-big-picutre.js
+++ js/open-big-picutre.js
@@ -100,6 +100,5 @@ const onClickPicture = (evt) => {
     openBigPicture();
   }
 };
-
 buttonCommentsLoader.removeEventListener('click', onCommentsLoaderClick);
 picturesContainer.addEventListener('click', onClickPicture);
