f714bb7cd9c75379ddb8f2b73f07c122673b8538
simon <semyon.dudin@inbox.ru>
2024-11-30 14:38:00 +0300

    Фото на память

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
 import './open-big-picutre.js';
+import './load-picture.js';
 import { onCloseFrom, setUserFormSubmit } from './user-form.js';
 import { getData } from './api.js';
 import { onSuccess, onError } from './api-util.js';
+
 
 getData()
   .then((pictures) => {
